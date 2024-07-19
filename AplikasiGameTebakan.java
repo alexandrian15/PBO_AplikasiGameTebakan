d078fe0eead2a1371af1af33ce81eeb5710b33b9
alexandrian15 <alexandrian6186@gmail.com>
2024-07-19 17:35:21 +0700

    Add files via upload

---
+++ AplikasiGameTebakan.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aplikasigametebakan;
+
+/**
+ *
+ * @author USER
+ */
+public class AplikasiGameTebakan {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.start();
+    }
+}
